31c8d8ea7da42f918893f5cd7f54c355d09b1393
sidav <sidav94@gmail.com>
2022-02-15 21:52:01 +0300

    WIP.

--- geometry/geometry.go
+++ geometry/geometry.go
@@ -60,71 +60,8 @@ func OrthogonalDistance(x1, y1, x2, y2 int) int {
 	return max(abs(x1-x2), abs(y1-y2))
 }
 
-func SquareContainsCoords(sx, sy, w, x, y int) bool {
-	return RectContainsCoords(sx, sy, w, w, x, y)
-}
-
-func RectContainsCoords(rx, ry, w, h, x, y int) bool {
-	return rx <= x && x < rx+w && ry <= y && y < ry+h
-}
-
-func DoTwoSquaresOverlap(x1, y1, w1, x2, y2, w2 int) bool {
-	return DoTwoCellRectsOverlap(x1, y1, w1, w1, x2, y2, w2, w2)
-}
-
-func DoTwoCellRectsOverlap(x1, y1, w1, h1, x2, y2, w2, h2 int) bool {
-	// WARNING:
-	// ALL "-1"s HERE ARE BECAUSE OF WE ARE IN CELLS SPACE
-	// I.E. A SINGLE CELL IS 1x1 RECTANGLE
-	// SO RECTS (0, 0, 1x1) AND (1, 0, 1x1) ARE NOT OVERLAPPING IN THIS SPACE (BUT SHOULD IN EUCLIDEAN OF COURSE)
-	right1 := x1 + w1 - 1
-	bottom1 := y1 + h1 - 1
-	right2 := x2 + w2 - 1
-	bottom2 := y2 + h2 - 1
-	return !(x2 > right1 ||
-		right2 < x1 ||
-		y2 > bottom1 ||
-		bottom2 < y1)
-}
-
-func DistanceBetweenSquares(x1, y1, w1, x2, y2, w2 int) int {
-	return DistanceBetweenRectangles(x1, y1, w1, w1, x2, y2, w2, w2)
-}
-
-func DistanceBetweenRectangles(x1, y1, w1, h1, x2, y2, w2, h2 int) int {
-	w1--
-	h1--
-	w2--
-	h2--
-	left := x2+w2 < x1
-	right := x1+w1 < x2
-	bottom := y2+h2 < y1
-	top := y1+h1 < y2
-	if top && left {
-		return OrthogonalDistance(x1, y1+h1, x2+w2, y2)
-	}
-	if left && bottom {
-		return OrthogonalDistance(x1, y1, x2+w2, y2+h2)
-	}
-	if bottom && right {
-		return OrthogonalDistance(x1+w1, y1, x2, y2+h2)
-	}
-	if right && top {
-		return OrthogonalDistance(x1+w1, y1+h1, x2, y2)
-	}
-	if left {
-		return x1 - x2 - w2
-	}
-	if right {
-		return x2 - x1 - w1
-	}
-	if bottom {
-		return y1 - y2 - h2
-	}
-	if top {
-		return y2 - y1 - h1
-	}
-	return 0 // intersect
+func sqDistance(x1, y1, x2, y2 int) int {
+	return abs(x1-x2)*abs(x1-x2) + abs(y1-y2)*abs(y1-y2)
 }
 
 func ScaleCoords(x, y, scale int) [][]int {
@@ -135,16 +72,4 @@ func ScaleCoords(x, y, scale int) [][]int {
 		}
 	}
 	return scaled
-}
-
-func MoveSquareByVector(vx, vy, topleftx, toplefty, size int) [][]int {
-	var rectCoords [][]int
-	newtopleftx := topleftx + vx*size
-	newtoplefty := toplefty + vy*size
-	for x := 0; x < size; x++ {
-		for y := 0; y < size; y++ {
-			rectCoords = append(rectCoords, []int{newtopleftx + x, newtoplefty + y})
-		}
-	}
-	return rectCoords
 }
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"github.com/sidav/sidavgorandom/fibrandom"
-	"math"
 	"soulsrl/game_log"
 	"soulsrl/geometry"
 )
@@ -16,16 +15,7 @@ var (
 )
 
 func main() {
-	x, y := 2, 0
-	fx, fy := float64(x), float64(y)
-	for i := 0; i <= 8; i++ {
-		fmt.Printf("Rotated: %d, %d == %.1f, %.1f\n", x, y, fx, fy)
-		// x, y = rotateIntVector(x, y, 45)
-		x, y = geometry.StupidRotateVector45(x, y)
-		ft := fx
-		fx = fx*math.Cos(math.Pi/4) - fy*math.Sin(math.Pi/4) + 0.1
-		fy = ft*math.Sin(math.Pi/4) + fy*math.Cos(math.Pi/4) + 0.1
-	}
+	doTestingStuff()
 
 	log = &game_log.GameLog{}
 	log.Init(3)
@@ -40,4 +30,27 @@ func main() {
 	for !exitGame {
 		b.combatGameLoop()
 	}
+}
+
+func doTestingStuff() {
+	//x, y := 2, 0
+	//fx, fy := float64(x), float64(y)
+	//for i := 0; i <= 8; i++ {
+	//	fmt.Printf("Rotated: %d, %d == %.1f, %.1f\n", x, y, fx, fy)
+	//	// x, y = rotateIntVector(x, y, 45)
+	//	x, y = geometry.StupidRotateVector45(x, y)
+	//	ft := fx
+	//	fx = fx*math.Cos(math.Pi/4) - fy*math.Sin(math.Pi/4) + 0.1
+	//	fy = ft*math.Sin(math.Pi/4) + fy*math.Cos(math.Pi/4) + 0.1
+	//}
+	x, y := 0, 0
+	size := 3
+	//for atx := 0; atx < 10; atx++ {
+	//	for aty := 0; aty < 10; aty++ {
+	//		fmt.Printf("Dist to %d, %d: %d\n", atx, aty, geometry.DistanceBetweenSquares(x, y, size, atx, aty, size))
+	//	}
+	//}
+	atx, aty := 3, 3
+	_, foundx, foundy := geometry.FindCoordsForNeighbouringSquareOfSameSizeContainingCoords(x, y, size, atx, aty)
+	fmt.Printf("Closest: %d, %d\n", foundx, foundy)
 }
